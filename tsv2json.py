2e027321b940c138f1decc32e282a5e486ff5309
Kaushik Sadhu <st158332@stud.uni-stuttgart.de>
2020-09-06 15:06:17 +0200

    Add files via upload

--- tsv2json.py
+++ tsv2json.py
@@ -1,8 +1,17 @@
+# Author: Kaushik Sadhu
+# Script for TSV to JSON format conversion
+# IMS - University of Stuttgart 15.08.2020
+# Updated: September 06/2020
+# filename = enter the tsv filename in cmd when you start conversion command by calling python tsv2Json.py "filename"
+
+
 # Import lib
 
 import csv
 import json
 import collections
+import sys
+
 
 # aliases
 OrderedDict = collections.OrderedDict
@@ -13,12 +22,14 @@ dst = '/tmp/data.json'
 # data input output
 
 data=[{}]
-with open('data.json', 'w') as outfile, open("data.tsv","r") as f:
-firstline = f.readline()
-columns = firstline.split()
-lines = f.readlines()[1:]
+filename = sys.argv[1]
+with open(filename,"r") as f:
+    firstline = f.readline()
+    columns = firstline.split()
+    lines = f.readlines()[1:]
 for line in lines:
     values = line.split()
     entry = dict(zip(columns, values))
     data.append(entry)
-json.dump(data, outfile)
+with open('output_TSV2JSON.json', 'w') as outfile:
+    json.dump(data, outfile)
